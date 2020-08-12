a290d78beb921e6799cc6696f8b03b1f25f134d8
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-08-11 20:30:21 -0400

    expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,21 @@
 # Your code here
-
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    global cache
+    return_value = 0
+    key = f'{x}, {y}, {z}'
+    if key in cache:
+        return cache[key]
+    elif x <= 0:
+        return_value = y + z
+    else:
+        return_value = expensive_seq(x - 1, y - 1, z)\
+            + expensive_seq(x - 2, y + 2, z * 2)\
+            + expensive_seq(x - 3, y + 3, z * 3)
 
-
+    cache[key] = return_value
+    return return_value
 
 if __name__ == "__main__":
     for i in range(10):
